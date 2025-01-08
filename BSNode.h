eb3c32709ddc173f187bd9a1556a3e52216215ef
Isaac Perez Mascaro <ipermas@upv.edu.es>
2025-01-08 18:14:40 +0100

    Añadida implementación de la clase BSNode

--- BSNode.h
+++ BSNode.h
@@ -2,7 +2,6 @@
 #define BSNODE_H
 
 #include <ostream>
-using namespace std;
 
 template <typename T> 
 class BSNode {
@@ -11,17 +10,17 @@ class BSNode {
 		BSNode<T>* left;
 		BSNode<T>* right;
 		
-		BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr) {
+		BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr){
 			this->elem = elem;
 			this->left = left;
 			this->right = right;
 		}
 		
-		friend ostream& operator<<(ostream &out, const BSNode<T> &bsn) {
+		friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn){
 			out << bsn.elem;
 			return out;
 		}
 };
+     
 
 #endif
-
