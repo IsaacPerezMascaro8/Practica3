5caaeb28043466474563fe60ccc22d1bf665ed55
Isaac Perez Mascaro <ipermas@upv.edu.es>
2025-01-08 18:10:58 +0100

    Añadida interfaz genérica Dict

--- Dict.h
+++ Dict.h
@@ -1,26 +1,16 @@
 #ifndef DICT_H
 #define DICT_H
-
 #include <string>
-#include <stdexcept>
-
-using namespace std;
 
 template <typename V>
-class Dict {
-public:
-    // Inserta el par key->value en el diccionario.
-    virtual void insert(string key, V value) = 0;
 
-    // Busca el valor correspondiente a key. Devuelve el valor asociado si key está en el diccionario.
-    virtual V search(string key) = 0;
-
-    // Elimina el par key->value si se encuentra en el diccionario.
-    virtual V remove(string key) = 0;
-
-    // Devuelve el número de elementos que tiene el diccionario.
-    virtual int entries() = 0;
+class Dict{
+	public:
+		virtual ~Dict() = default;
+		virtual void insert(std::string key, V value) = 0;
+		virtual V search(std::string key) const = 0;
+		virtual V remove(std::string key) = 0;
+		virtual int entries() const = 0;
 };
 
 #endif
-
